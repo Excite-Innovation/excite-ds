284564b9885c396cff4520f9a2484753631af6b5
Paul Mbingu <paulxmbingu@gmail.com>
2026-06-21 11:09:06 +0300

    feat: implement navigation menu and home layout components

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -12,6 +12,17 @@ export default function Layout({ children }: LayoutProps<"/docs">) {
       containerProps={{ className: "..." }}
       // tree={source.getPageTree()}
       // links={[]}
+      tabs={[
+        {
+          title: 'Componentsh',
+          description: 'Hello World!',
+          // active for `/docs/components` and sub routes like `/docs/components/button`
+          url: '/docs/components',
+          // optionally, you can specify a set of urls which activates the item
+          // urls: new Set(['/docs/test', '/docs/components']),
+          
+        },
+      ]}
     >
       {children}
     </DocsLayout>
